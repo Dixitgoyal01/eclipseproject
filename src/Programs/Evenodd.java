ee2f0864c1e70f68c36ad97a1cdb17c4d117e76c
Dixitgoyal01 <49027887+Dixitgoyal01@users.noreply.github.com>
2019-04-01 20:43:40 +0530

    first commit

---
+++ src/Programs/Evenodd.java
@@ -0,0 +1,31 @@
+package Programs;
+
+public class Evenodd {
+	
+	public static void main(String[]arug) {
+		
+		int a=50;
+		int b=800;
+		int c=200;
+		if(a>b && a>c  )
+		{ 
+		System.out.print("a is greater than b & c");
+		}
+		else 
+		{
+		System.out.print("a is smallest than c ");
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ src/Programs/Primeodd.java
@@ -0,0 +1,31 @@
+package Programs;
+import java.util.Scanner;
+public class Primeodd
+{
+public static void main(String[] arug) {
+	
+	
+	
+	Scanner scn=new Scanner(System.in);
+	System.out.println("Enter the number");
+	int a= scn.nextInt();
+	
+			
+	
+	if(a%2==0)
+		
+	{	
+	System.out.println("is prime number");}
+	else
+	{System.out.println("odd number");}
+	
+	
+}
+
+
+
+
+	
+	
+	
+}
